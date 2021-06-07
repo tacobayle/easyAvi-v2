e574d449bdb867f244b01a285e427740da42ad31
tacobayle <nicolas.bayle@gmail.com>
2021-06-07 13:49:21 +0000

    20210527

--- se_vmc/variables.tf
+++ se_vmc/variables.tf
@@ -4,3 +4,6 @@ variable "vmc_vsphere_username" {}
 variable "vmc_vsphere_password" {}
 variable "vmc_vsphere_server" {}
 variable "no_access_vcenter" {}
+variable "vmc_sddc_id" {}
+variable "vmc_org_id" {}
+variable "vmc_nsx_token" {}
